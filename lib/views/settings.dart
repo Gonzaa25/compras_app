ba1e67fb48e918d064f94273dfb6f82f15f36d57
Gonzaa25 <gonzalo.gauto@gmail.com>
2020-12-16 19:13:33 -0300

    add settings page

---
+++ lib/views/settings.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  //TODO: implement theme controller
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
